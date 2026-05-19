53b18552789b1e17abcf95a6624701a7bb6dd03a
RWelleman <r.p.j.welleman@student.hhs.nl>
2026-05-19 20:53:38 +0200

    Bevestigingsmail wordt verstuurd na invullen form

--- Allfit-Webproject.Server/Controllers/FormController.cs
+++ Allfit-Webproject.Server/Controllers/FormController.cs
@@ -1,6 +1,8 @@
 ﻿using Allfit_Webproject.Server.Dtos;
 using Allfit_Webproject.Server.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
 
 namespace Allfit_Webproject.Server.Controllers
 {
@@ -9,6 +11,8 @@ namespace Allfit_Webproject.Server.Controllers
     public class FormController : ControllerBase
     {
         private readonly IFormService _service;
+        private readonly String mailTrapToken = "8ebfb0ad5d1ac972bd0ed249699cc57e";
+        private readonly String mailDomain = "hello@demomailtrap.co";
 
         public FormController(IFormService formService)
         {
@@ -21,9 +25,26 @@ namespace Allfit_Webproject.Server.Controllers
             // Write to database
             await _service.SaveFormAsync(formData);
 
-            // Send confirmation email
-
-            Console.WriteLine(formData.Naam);
+            // Send confirmation email wegens testomgeving alleen mogelijk naar r.p.j.welleman@student.hhs.nl
+            try
+            {
+                MailMessage message = new MailMessage(
+                    mailDomain,
+                    formData.Email,
+                    "Bevestiging van uw aanvraag",
+                    $"Beste {formData.Naam},\n\nBedankt voor uw aanvraag. We nemen zo snel mogelijk telefonisch contact met u op via {formData.Telefoon}.\n\nMet vriendelijke groet,\nAllfit Team"
+                    );
+                SmtpClient client = new SmtpClient("live.smtp.mailtrap.io", 587)
+                {
+                    Credentials = new NetworkCredential("api", mailTrapToken),
+                    EnableSsl = true
+                };
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
         }
     }
 }
