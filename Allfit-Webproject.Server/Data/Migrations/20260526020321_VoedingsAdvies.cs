955c3b02035fa512d56583c633f0cc9d46076367
saksagan86 <bgunenc0@gmail.com>
2026-05-26 05:13:46 +0200

    Voedingsadvies implementatie, logo gemaakt en verandert, routing verandert van account pagina

---
+++ Allfit-Webproject.Server/Data/Migrations/20260526020321_VoedingsAdvies.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Allfit_Webproject.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VoedingsAdvies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doelen",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    naam = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    beschrijving = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    actief = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doelen", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GebruikerDoelen",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    gebruikerId = table.Column<int>(type: "int", nullable: false),
+                    doelId = table.Column<int>(type: "int", nullable: false),
+                    aangemaaktOp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    gewijzigdOp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GebruikerDoelen", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_GebruikerDoelen_Doelen_doelId",
+                        column: x => x.doelId,
+                        principalTable: "Doelen",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GebruikerDoelen_Gebruiker_gebruikerId",
+                        column: x => x.gebruikerId,
+                        principalTable: "Gebruiker",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voedingsschemas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    doelId = table.Column<int>(type: "int", nullable: false),
+                    titel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    beschrijving = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voedingsschemas", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Voedingsschemas_Doelen_doelId",
+                        column: x => x.doelId,
+                        principalTable: "Doelen",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VoedingsschemaRegels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    voedingsschemaId = table.Column<int>(type: "int", nullable: false),
+                    maaltijdMoment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    advies = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    volgorde = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VoedingsschemaRegels", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_VoedingsschemaRegels_Voedingsschemas_voedingsschemaId",
+                        column: x => x.voedingsschemaId,
+                        principalTable: "Voedingsschemas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GebruikerDoelen_doelId",
+                table: "GebruikerDoelen",
+                column: "doelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GebruikerDoelen_gebruikerId",
+                table: "GebruikerDoelen",
+                column: "gebruikerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VoedingsschemaRegels_voedingsschemaId",
+                table: "VoedingsschemaRegels",
+                column: "voedingsschemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voedingsschemas_doelId",
+                table: "Voedingsschemas",
+                column: "doelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GebruikerDoelen");
+
+            migrationBuilder.DropTable(
+                name: "VoedingsschemaRegels");
+
+            migrationBuilder.DropTable(
+                name: "Voedingsschemas");
+
+            migrationBuilder.DropTable(
+                name: "Doelen");
+        }
+    }
+}
--- allfit-webproject.client/src/App.jsx
+++ allfit-webproject.client/src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar from './Components/Navbar.jsx'
+import Navbar from './Components/Navbar.jsx';
 import ProtectedRoute from './Components/ProtectedRoute.jsx';
 import ScrollNaarBoven from './Components/ScrollNaarBoven';
+
 import InschrijfPage from './Pages/InschrijfPage';
-import HomePage from './Pages/HomePage.jsx'
-import ContactPage from './Pages/ContactPage.jsx'
-import LocatiesPage from './Pages/LocatiesPage.jsx'
-import NotFoundPage from './Pages/NotFoundPage.jsx'
-import AanbodOverzicht from './Components/AanbodOverzicht';
+import HomePage from './Pages/HomePage.jsx';
+import ContactPage from './Pages/ContactPage.jsx';
+import LocatiesPage from './Pages/LocatiesPage.jsx';
+import NotFoundPage from './Pages/NotFoundPage.jsx';
 import DetailPagina from './Pages/DetailPagina';
 import LoginPage from './Pages/LoginPage.jsx';
 import AccountPage from './Pages/AccountPage.jsx';
@@ -18,18 +18,22 @@ import ExtraoptiesPagina from './Pages/ExtraoptiesPagina';
 import LessenPage from './Pages/LessenPage';
 import AccountOverzicht from './Pages/AccountOverzicht';
 import CommunityPage from './Pages/CommunityPage.jsx';
+import VoedingsadviesPage from './Pages/VoedingsadviesPage.jsx';
 
+import AanbodOverzicht from './Components/AanbodOverzicht';
 
 function App() {
     return (
         <div>
             <ScrollNaarBoven />
+
             <link
                 href="https://fonts.googleapis.com/css2?family=Noto+Sans&family=Roboto:wght@700&display=swap"
                 rel="stylesheet"
             />
 
             <Navbar />
+
             <div className="page-content">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
@@ -41,11 +45,16 @@ function App() {
                     <Route path="/inschrijven" element={<InschrijfPage />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/community" element={<CommunityPage />} />
-                    <Route path="/lessen/:aanbodId" element={
-                        <ProtectedRoute>
-                            <LessenPage />
-                        </ProtectedRoute>
-                    } />
+
+                    <Route
+                        path="/lessen/:aanbodId"
+                        element={
+                            <ProtectedRoute>
+                                <LessenPage />
+                            </ProtectedRoute>
+                        }
+                    />
+
                     <Route
                         path="/account"
                         element={
@@ -54,11 +63,25 @@ function App() {
                             </ProtectedRoute>
                         }
                     />
-                    <Route path="/account/overzicht" element={
-                        <ProtectedRoute>
-                            <AccountOverzicht />
-                        </ProtectedRoute>
-                    } />
+
+                    <Route
+                        path="/account/overzicht"
+                        element={
+                            <ProtectedRoute>
+                                <AccountOverzicht />
+                            </ProtectedRoute>
+                        }
+                    />
+
+                    <Route
+                        path="/voedingsadvies"
+                        element={
+                            <ProtectedRoute>
+                                <VoedingsadviesPage />
+                            </ProtectedRoute>
+                        }
+                    />
+
                     <Route path="*" element={<NotFoundPage />} />
                 </Routes>
             </div>
