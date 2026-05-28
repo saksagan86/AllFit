e119dc5f15a764981485b3fb9da01dfd7c57c499
saksagan86 <bgunenc0@gmail.com>
2026-05-28 09:34:05 +0200

    update voedingsadvies

---
+++ Allfit-Webproject.Server/Repository/IVoedingsadviesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Allfit_Webproject.Server.Models;
+
+namespace Allfit_Webproject.Server.Repository
+{
+    public interface IVoedingsadviesRepository
+    {
+        Task<List<Doel>> HaalActieveDoelenOpAsync();
+        Task<GebruikerDoel?> HaalGebruikerDoelMetSchemaOpAsync(int gebruikerId);
+        Task<bool> DoelBestaatEnIsActiefAsync(int doelId);
+        Task<GebruikerDoel?> HaalGebruikerDoelOpAsync(int gebruikerId);
+        Task VoegGebruikerDoelToeAsync(GebruikerDoel gebruikerDoel);
+        Task SaveChangesAsync();
+    }
+}
